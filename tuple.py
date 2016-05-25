c1a887a125acf99f9b0b175a35471a6a59160ea1
panpxl <121959631@qq.com>
2016-05-25 10:59:54 +0800

    first

--- tuple.py
+++ tuple.py
@@ -1,3 +1,14 @@
 classmates = ('Michael', 'Bob', 'Tracy')
 print(classmates)
 print(classmates[0],classmates[1],classmates[2])
+
+#练习
+L = [
+    ['Apple', 'Google', 'Microsoft'],
+    ['Java', 'Python', 'Ruby', 'PHP'],
+    ['Adam', 'Bart', 'Lisa']
+]
+print(L[0][0])
+print(L[1][1])
+print(L[2][2])
+print(L[1][3])
---
+++ 循环.py
@@ -0,0 +1,16 @@
+names = ['Michael', 'Bob', 'Tracy']
+for name in names:
+    print(name)
+s=0
+L = list(range(101))
+for M in L:
+    s = s + M
+print(s)
+
+
+sum = 0
+n = 99
+while n > 0:
+    sum = sum + n
+    n = n - 2
+print(sum)
