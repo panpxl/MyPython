e173c1af08543b66ada1c963b93c056057e87eec
panpxl <121959631@qq.com>
2016-05-25 21:44:39 +0800

    first

--- dict.py
+++ dict.py
@@ -3,3 +3,12 @@
 
 names = ['Michael', 'Bob', 'Tracy']
 scores = [95, 75, 85]
+d = {'Michael': 95, 'Bob': 75, 'Tracy': 85}
+print(d['Michael'])
+d['Adam']=67
+print(d)
+print('Thomas' in d)
+print(d.get('Thomas',-1))
+print(d)
+d.pop('Bob')
+print(d)
