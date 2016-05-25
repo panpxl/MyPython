565abf39e692abf388a209fcb523018cad7b4b0f
panpxl <121959631@qq.com>
2016-05-25 13:43:04 +0800

    first

---
+++ dict.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+names = ['Michael', 'Bob', 'Tracy']
+scores = [95, 75, 85]
--- 循环.py
+++ 循环.py
@@ -1,16 +1,15 @@
 names = ['Michael', 'Bob', 'Tracy']
 for name in names:
     print(name)
-s=0
+s = 0
 L = list(range(101))
 for M in L:
-    s = s + M
+    s += M
 print(s)
-
 
 sum = 0
 n = 99
 while n > 0:
     sum = sum + n
-    n = n - 2
+    n -= 2
 print(sum)
