87991fbe01601149217fc1cdf2d212bf03df159f
panpxl <121959631@qq.com>
2016-05-27 15:08:25 +0800

    first

--- set.py
+++ set.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# eric,普通,123
+# tony,超级,1234
+# alex,超神,12345
 
-print('---------------星辰工作室---------------')
-guess=int(temp)
-if guess==8:
-    print()
+sum = 0
+sa=range(5,115,5)
+for x in sa:
+    sum+=x
+print(sum)
+
+
+L=[]
+for x in range(1, 15):
+    L.append(x ** x)
+print(L)
